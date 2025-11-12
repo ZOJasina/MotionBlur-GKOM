7abcfce478b11e729c8ee1f95f2c45d2c651d813
mkaniew2 <01178533@pw.edu.pl>
2025-11-12 18:06:46 +0100

    reversed car

--- car.py
+++ car.py
@@ -62,6 +62,7 @@ class Car:
         model = glm.mat4(1.0)
         model = glm.translate(model, self.position)
         model = glm.rotate(model, self.angle, glm.vec3(0.0, 1.0, 0.0))
+        model = glm.rotate(model, glm.radians(180.0), glm.vec3(0.0, 1.0, 0.0))
         model = glm.scale(model, glm.vec3(0.2))
 
         return model
